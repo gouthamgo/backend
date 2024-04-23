5cc1b2d7fc93a45a4f09f4764e8bbb2a3d6cbf5e
Goutham M <gouthamgo@yahoo.com>
2024-04-23 23:41:13 +1000

    js

---
+++ 12jsconcepts.js
@@ -0,0 +1,21 @@
+// Var Example 
+var age = 35; 
+console.log(age); 
+
+// Variable can be redeclared and updated 
+var age = 10; 
+console.log(age); 
+
+// Let Example 
+
+let name = "test"; 
+console.log(name); 
+
+// Variable can be reassigned 
+name = "wont work"; 
+console.log(name);
+
+// Var – Once a declared using the ‘var’ keyword, it can be updated and reassigned in the scope many times.
+// let – Once a variable is created using the ‘let’ keyword, it can be updated but cannot be redeclared.
+// const – Once a variable is created with the ‘const’ keyword, it can neither be updated, nor redeclared.
+
